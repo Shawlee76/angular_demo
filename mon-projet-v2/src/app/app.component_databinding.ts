bda476de8a0b8162475126bbc6672947ac8f7162
shawlee76 <shawlee76@gmail.com>
2018-07-30 16:53:32 +0200

    Utilisation des directives struturelles et par attribut dans Angular

--- mon-projet-v2/src/app/app.component_databinding.ts
+++ mon-projet-v2/src/app/app.component_databinding.ts
@@ -9,20 +9,9 @@ export class AppComponent {
   title = 'Mon Projet Angular V2 - wow !';
   isAuth = false;
 
-  appareils = [
-    {
-      name: 'Machine à laver',
-      status: 'éteint'
-    },
-    {
-      name: 'Frigo',
-      status: 'allumé'
-    },
-    {
-      name: 'Ordinateur',
-      status: 'éteint'
-    }
-  ];
+  appareilOne = 'Machine à laver';
+  appareilTwo = 'Frigo';
+  appareilThree = 'Ordinateur';
 
   constructor() {
     setTimeout(
